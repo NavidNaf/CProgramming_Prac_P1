ce4955a0c84660184212b0addec10366d8b8bdde
NavidNaf <33064619+NavidNaf@users.noreply.github.com>
2020-05-28 20:14:04 +0600

    Solution to CS50 W1 Assignment

---
+++ CS50_W1_Solution/mariomore.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int i, j, k, n;
+    do
+    {
+        n = get_int("Height: ");
+    } while (n < 1 || n > 8);
+    for (i = 1; i <= n; i++)
+    {
+        for (k = n; k > i; k--)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int m = 1; m <= i; m++)
+        {
+
+            printf("#");
+        }
+        printf("\n");
+    }
+}
